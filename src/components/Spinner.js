b775a46584a443372b1edce5b62897249c78a0c8
Harsh Pundir <harshpundir658@gmail.com>
2024-01-27 17:36:54 +0530

    commit

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import loading from './rainbow-spinner.gif'
 const Spinner=()=>{
       return(
         <div className='text-center'>
-            <img src={loading} alt='loading'/>
+            <img src={loading} alt='loading' height={40} width={50}/>
         </div>
       )
 }
